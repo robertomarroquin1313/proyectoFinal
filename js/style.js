c3037110c94fb62c553268562803a2e023537e7c
Roberto Francisco Marroquín Osorio <67878026+Roberto1313@users.noreply.github.com>
2023-05-28 19:41:52 -0600

    Animacion automatica de swiper

--- js/style.js
+++ js/style.js
@@ -14,10 +14,9 @@ function initializeSwiper(){
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
+        autoplay: {
+            delay: 3000, // Tiempo en milisegundos entre cada cambio de slide
+            disableOnInteraction: false, // Permite la interacción del usuario con el swiper mientras se está reproduciendo automáticamente
         },
       });
 }
